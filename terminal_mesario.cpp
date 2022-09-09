a2835c58d63cfdb40932414fef5bc952be12ea3c
cardoso <jefersoncardoso.dev@gmail.com>
2022-09-09 15:21:17 -0400

    comentarios
    comentarios de ajuda

--- terminal_mesario.cpp
+++ terminal_mesario.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
 #include <string>
 
-#include "digite.h"
-#include "entrada.h"
-#include "zeressima.h"
-#include "registro_mesarios.h"
+#include "digite.h"                /* Entrada padrão e mascara do campo titulo  */
+#include "entrada.h"              /* Responsavel pela identificação dos eleitores  */
+#include "zeressima.h"           /* imprima todos candidatos, sem nenhum voto   */
+#include "registro_mesarios.h"  /* Arquivo de registro de messarios  */
 
-#define SECAO 234
-#define ZONA 123
-#define UF MT
-#define N 13
+#define SECAO 234               /* Sessão modelo  */
+#define ZONA 123               /* Zona eleitoral  */
+#define UF MT                 /* União deferativa */
+#define N 13                                             //tamanho do array de char
 using namespace std;
-char numeroIdentificacao[N];
+char numeroIdentificacao[N];                              // Array para o titulo
 
 void limparTela(){
     system("clear");
 }
 
 
-
+/*-----------Metodo destinado a cadastrar titulo--------------*/
 void titulo_eleitor(){
      int tituloEleitor=0;
      char numeroTitulo;
-      tituloMascara(numeroIdentificacao);
-     while (tituloEleitor < sizeof(numeroIdentificacao) ) { 
-        limparTela();
-           exibir(numeroIdentificacao);
+      tituloMascara(numeroIdentificacao);                    // mascara demostrativa: [ _ _ _ _ _ _ _ _ _ _ ]
+     while (tituloEleitor < sizeof(numeroIdentificacao) ) { //  percorre o array   
+        limparTela();                                       //  mantera tela limpa
+           exibir(numeroIdentificacao);                    //   mostre valores digitados
           
           cout << "\n= ";
-          cin >> numeroTitulo;
+          cin >> numeroTitulo;                            // input, entrada da informação
           
-          numeroIdentificacao[tituloEleitor] = numeroTitulo;
-          tituloEleitor++;
+          numeroIdentificacao[tituloEleitor] = numeroTitulo; //adiciona ao array a entrada informada
+          tituloEleitor++;                               
         }
-      terminal(numeroIdentificacao );
+      terminal(numeroIdentificacao );                      //metodo mostre os eleitores da sessão, averigua se pertence a sessao
      }
 
+
+
+
+//------------------- Metodo main --------------------------
 int main(){
     string cadidato[4][5]; 
   int preparativos;
