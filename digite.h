f653cc571b6e32b175529b29bcc41d2f31514c49
cardoso <jefersoncardoso.dev@gmail.com>
2022-09-09 15:23:32 -0400

    comentario#2

--- digite.h
+++ digite.h
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <string>
-#define N 13
+#define N 13  /* tamanho do vetor */
 
 using namespace std;
 
 
 
 
-
+//Metodo delegado a mensagem
 void exibir(char numeroIdentificacao[N]){
   cout << "\n[ >> ] - Infome o numero do titulo do eleitor\n";
     cout << "[ ";
@@ -19,7 +19,7 @@ void exibir(char numeroIdentificacao[N]){
     
         
 }
-
+ /* Mascara de apresentação  */
 void tituloMascara(char numeroIdentificacao[N]){
    
     for(int linha=0 ; linha < N; linha++){
